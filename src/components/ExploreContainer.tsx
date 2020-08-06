9a6f145a7172f90e97abbfa622926080d2d99c16
Adrian Gonzalo <gonzahh@gmail.com>
2020-08-06 23:33:32 +0200

    added selector for number of items used in the password

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,17 +1,47 @@
 import React, { useState } from 'react';
 import './ExploreContainer.css';
-import { IonItem, IonButton, IonInput } from '@ionic/react';
-import { generatePass } from "../passgen.bundle"
+import {
+  IonList,
+  IonItem,
+  IonButton,
+  IonInput,
+  IonRadioGroup,
+  IonRadio,
+  IonLabel,
+  IonRange
+} from '@ionic/react';
+import{ generatePass } from "../passgen.bundle"
+
+type PassType = "alpha" | "num" | "alphanum" | "alphanumExt" | "words-es" | "words-en" | "pokemon-1st"
+
+type PassStrength = "very weak" | "weak" | "reasonable" | "strong" | "very strong" | "overkill"
+
+interface PassOptions {
+  type: PassType
+  number: number
+  caps: boolean
+}
+
+interface PassData extends PassOptions {
+  pass: string
+  entropy: number
+  strength: PassStrength
+  relativeEntropy: number
+  relativeStrength: PassStrength
+}
 
 interface ContainerProps { }
 
 const PasswordGenerator: React.FC<ContainerProps> = () => {
+  const types = ["alpha", "num", "alphanum", "alphanumExt", "words-es", "words-en", "pokemon-1st"]
   const [password, setPassword] = useState("")
+  const [type, setType] = useState("words-es")
+  const [number, setNumber] = useState(8)
 
   const generate = () => {
     const data = generatePass({
-      type: "alpha",
-      number: 10,
+      type,
+      number,
       caps: true,
     })
     const { pass } = data
@@ -20,14 +50,41 @@ const PasswordGenerator: React.FC<ContainerProps> = () => {
   }
 
   return (
-    <div>
+    <IonList>
+      <IonRadioGroup value={type} onIonChange={e => setType(e.detail.value)}>
+        {types.map(item => (
+          <IonItem key={item}>
+            <IonLabel>{item}</IonLabel>
+            <IonRadio slot="start" value={item}></IonRadio>
+          </IonItem>
+        ))}
+      </IonRadioGroup>
       <IonItem lines="none">
-        <IonButton size="default" onClick={() => generate()}>Generate Random Password</IonButton>
+        <IonRange
+          min={4}
+          max={100}
+          value={number}
+          onIonChange={e => setNumber(e.detail.value as number)}
+          debounce={200}
+          step={1}
+          pin
+          snaps
+          ticks
+        >
+          <IonLabel slot="start">0</IonLabel>
+          <IonLabel slot="end">100</IonLabel>
+        </IonRange>
+      </IonItem>
+      <IonItem lines="none">
+        <IonButton
+          size="default"
+          onClick={() => generate()}
+        >Generate Random Password</IonButton>
       </IonItem>
       <IonItem lines="none">
         <IonInput type="text" value={password} disabled></IonInput>
       </IonItem>
-    </div>
+    </IonList>
   );
 };
 
